ec74dece5074100d316c648a08717c94b0cae2f1
mariliasantos21m <mariliasantosnas21@gmail.com>
2023-08-11 00:56:32 -0300

    feat:Alterando o front

--- lib/menu.dart
+++ lib/menu.dart
@@ -8,50 +8,61 @@ class MenuMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(237, 170, 0, 1),
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              );
-            },
+          appBar: AppBar(
+            backgroundColor: Color.fromRGBO(237, 170, 0, 1),
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+            title: Text('Depósito do Zé'),
           ),
-          title: Text('Depósito do Zé'),
-        ),
-        body: Container(
-            padding: EdgeInsets.all(30.0),
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: <Widget>[
-                CategoryCard('Cervejas'),
-                CategoryCard('Vinhos'),
-                CategoryCard('Destilados'),
-                CategoryCard('Cachaças'),
-                CategoryCard('Refrigerantes'),
-                CategoryCard('Outros'),
-              ],
-            )),
-      ),
+          body: Container(
+            color: Color.fromRGBO(237, 170, 0, 1),
+            child: Container(
+                margin: EdgeInsets.only(top: 20.0),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                ),
+                padding: EdgeInsets.all(30.0),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    CategoryCard('Cervejas', 'assets/beer.png'),
+                    CategoryCard('Vinhos', 'assets/wine.png'),
+                    CategoryCard('Destilados', 'assets/destilados.png'),
+                    CategoryCard('Cachaças', 'assets/liquor.png'),
+                    CategoryCard('Refrigerantes', 'assets/soft_drink.png'),
+                    CategoryCard('Outros', 'assets/others.png'),
+                  ],
+                )),
+          )),
     );
   }
 }
 
 class CategoryCard extends StatelessWidget {
   String categoria;
-  CategoryCard(this.categoria);
+  String img;
+  CategoryCard(this.categoria, this.img);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(8.0),
-      color: Color.fromARGB(255, 223, 223, 210),
-      elevation: 2,
+      margin: EdgeInsets.all(15.0),
+      color: Color.fromRGBO(238, 237, 237, 1),
+      elevation: 6,
       shape: RoundedRectangleBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
       ),
       child: InkWell(
         onTap: () {
@@ -60,15 +71,27 @@ class CategoryCard extends StatelessWidget {
             MaterialPageRoute(builder: (context) => CategoryScreen(categoria)),
           );
         },
-        splashColor: Color.fromRGBO(237, 170, 0, 1),
+        splashColor: Color.fromRGBO(255, 234, 182, 1),
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Icon(Icons.wine_bar_rounded, size: 40.0),
+              Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: Image(
+                    image: AssetImage(img),
+                    width: 50,
+                    height: 50,
+                    color: Color.fromRGBO(237, 170, 0, 1),
+                    fit: BoxFit.contain,
+                  )),
+              SizedBox(height: 10),
               Text(categoria,
                   style: new TextStyle(
-                      fontSize: 17.0, fontWeight: FontWeight.bold)),
+                      fontWeight: FontWeight.bold,
+                      fontFamily: AutofillHints.creditCardName,
+                      fontSize: 15.0,
+                      color: Color.fromRGBO(0, 0, 0, 1))),
             ],
           ),
         ),
