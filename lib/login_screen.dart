c9b1dc7e241a758a659eedb3186cbb38540a56e0
mariliasantos21m <mariliasantosnas21@gmail.com>
2023-11-12 17:12:12 -0300

    Ajustes na tela de login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'menu.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    final altura = MediaQuery.of(context).size.height;
+    final largura = MediaQuery.of(context).size.width;
+
     return Scaffold(
         body: Container(
+            height: altura,
+            width: largura,
             child: Container(
                 decoration: BoxDecoration(
                     gradient: LinearGradient(
@@ -24,16 +30,76 @@ class LoginScreen extends StatelessWidget {
                       Align(
                           alignment: Alignment(0.0, 8.0),
                           child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 10.0),
+                            padding: EdgeInsets.symmetric(vertical: 5.0),
                             child: Image(
-                                image: AssetImage('assets/logo_deposito.png')),
+                              image: AssetImage('assets/logo_deposito.png'),
+                              height: 300,
+                              width: 300,
+                            ),
                           )),
                       Align(
                           alignment: Alignment(0.0, 8.0),
-                          child: InputLogin('Usuario')),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 10),
+                                child: TextField(
+                                  autofocus: true,
+                                  keyboardType: TextInputType.number,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 15),
+                                  decoration: InputDecoration(
+                                    labelText: 'Usuário',
+                                    labelStyle: TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
                       Align(
                           alignment: Alignment(0.0, 8.0),
-                          child: InputLogin('Senha'))
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 10),
+                                child: TextField(
+                                  autofocus: true,
+                                  keyboardType: TextInputType.number,
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 15),
+                                  decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    labelStyle: TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 50),
+                          child: ElevatedButton(
+                              onPressed: () => {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MenuMain()),
+                                    ),
+                                  },
+                              child: Icon(
+                                Icons.arrow_right_alt_outlined,
+                                size: 40,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color.fromRGBO(237, 170, 0, 1),
+                                shape: StadiumBorder(),
+                                side: BorderSide(
+                                    color: Color.fromRGBO(237, 170, 0, 1),
+                                    width: 3),
+                              ))),
                     ])))));
   }
 }
@@ -50,16 +116,14 @@ class InputLogin extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
           child: TextField(
-              decoration: InputDecoration(
-                  border:
-                      OutlineInputBorder(borderSide: BorderSide(width: 3.0)),
-                  contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-                  hintText: placeHolder,
-                  fillColor: Color(0xFFFFFFFF),
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(237, 170, 0, 1), width: 3.0)))),
+            autofocus: true,
+            keyboardType: TextInputType.number,
+            style: TextStyle(color: Colors.black, fontSize: 20),
+            decoration: InputDecoration(
+              labelText: placeHolder,
+              labelStyle: TextStyle(color: Colors.black),
+            ),
+          ),
         ),
       ],
     );
